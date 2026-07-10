0e498c057219dd4c2712273afea56c56b196e09a
Samson Eshetu <sophosticated.me@gmail.com>
2026-07-10 16:54:40 +0300

    Initial commit - 42 Java drills from Day 1-2 practice

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,7 +1,22 @@
-public class Ex1BasicForLoop {
-    public static void main(String[] args) {
-        for(int i = 1; i <= 10; i++) {
-            System.out.println("Count: " + i);
-        }
+public class Book {
+    String title;
+    String author;
+    int pages;
+
+    public Book(String bookTitle, String bookAuthor, int bookPages) {
+        title = bookTitle;
+        author = bookAuthor;
+        pages = bookPages;
     }
+
+    public void showDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+    }
+    public static void main(String[] args) {
+        Book myBook = new Book("Java Basics", "John Doe", 300);
+        myBook.showDetails();
+    }
+
 }
